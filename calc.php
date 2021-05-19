beeb6ed3239f77446adf6b231ce7aa84923f4a7f
러리 <doralife12@naver.com>
2021-05-19 21:07:51 +0900

    Feat: calc.php

--- calc.php
+++ calc.php
@@ -31,13 +31,22 @@
 						{
 							$total = $_POST['number1'] / $_POST['number2'];	
 						}
+						if($_POST['operation'] == 'squared by') {
+							$total = $_POST['number1'] ** $_POST['number2'];
+						}
 						
 						echo "<h1>{$_POST['number1']} {$_POST['operation']} {$_POST['number2']} equals {$total}</h1>";
 					
+					} else if(is_numeric($_POST['number3'])) {
+						if($_POST['operation2'] == 'square root') {
+							$total2 = sqrt($_POST['number3']);
+						}
+						if($_POST['operation2'] == 'inverse') {
+							$total2 = 1 / $_POST['number3'];
+						}
+						echo "<h1>{$_POST['number3']} {$_POST['operation2']} equals {$total2}</h1>";
 					} else {
-						
 						echo 'Numeric values are required';
-					
 					}
 				}
 			?>
@@ -49,8 +58,17 @@
 		            <option value="minus">Minus</option>
 		            <option value="multiply">Multiply</option>
 		            <option value="divided by">Devide</option>
+		            <option value="squared by">Square</option>
 		        </select>
 		        <input name="number2" type="text" class="form-control" style="width: 150px; display: inline" />
+		        <input name="submit" type="submit" value="Calculate" class="btn btn-primary" />
+		    </form>
+
+		    <form method="post" action="calculator.php">
+		        <input name="number3" type="text" class="form-control" style="width: 150px; display: inline" />
+		        <select name="operation2">
+		            <option value="square root">SquareRoot</option>
+		        </select>
 		        <input name="submit" type="submit" value="Calculate" class="btn btn-primary" />
 		    </form>
 	    
